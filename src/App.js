d2a136276cb9fd7b8d2db89c7e93537b038cae36
dati giorgadze <83769327+niko940@users.noreply.github.com>
2023-03-10 19:28:00 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {GiHornedHelm} from 'react-icons/gi'
+import {AiOutlinePlus, AiOutlineClose} from 'react-icons/ai'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [tasks,setTask] = useState([])
+  const [input, setInput] = useState('')
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const addTask = {
+      id: Math.floor(Math.random() * 1000),
+      text:input,
+      completed:false
+    }
+    setTask([...tasks,addTask])
+    setInput('')
+
+  }
+
+  //delete tasks
+  const deleteTask =(id)=>{
+    let filteredTasks = [...tasks].filter((tasks) =>tasks.id !== id)
+    setTask(filteredTasks)
+    console.log('task deleted')
+  }
+
+  // toggle completed task
+  const toggleComplete = (id) =>{
+    setTask(
+      tasks.map(task => (
+        task.id === id ? {...task,completed: !task.completed} : task
+      ))
+    )
+  }
+
+  const date = new Date()
+  // console.log(date)
+  const days = ['Sunday', 'Monday','Tuesday','Wednesday', 'Thursday', 'Friday','Satureday']
+  const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+  return(
+    <div className='app'>
+      <div className="container">
+        <h1><GiHornedHelm/> ToDo For Discipline</h1>
+        
+        <div className='date'>
+          <p>{days[date.getDay()]}</p>
+          <p>{date.getDate()},</p>
+          <p>{months[date.getMonth()]}</p>
+          <p>{date.getFullYear()}</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className='form-input'>
+          <AiOutlinePlus className='icon'/>
+          <input
+          value={input}
+          onChange={e =>setInput(e.target.value)}
+          placeholder='Enter a task' 
+          type="text"></input>
+          </div>
+        </form>
+
+        <div>
+          {tasks.map(task => (
+            <div  className={`task-row ${task.completed? 'completed' :''}`} key={task.id} onClick={() => toggleComplete(task.id)}>
+              <p>{task.text}
+               </p>
+                  <AiOutlineClose onDoubleClick={()=> deleteTask(task.id)} className='icon'/>
+            </div>
+          ))}
+
+      </div>
+      <p className='length'>{(tasks < 1) ? 'You have no tasks' : `Tasks:${tasks.length}`}</p>
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
